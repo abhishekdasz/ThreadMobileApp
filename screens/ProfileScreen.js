214f6cee53a120709b6221dfae7d116505dafba8
Abhishek <abhishekdaszy@gmail.com>
2023-11-12 22:36:48 +0530

    Created Profile Screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,7 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import { UserType } from '../UserContext'
 
 const ProfileScreen = () => {
+    const [ user, setUser ] = useState("");
+    const { userId, setUserId } = useContext(UserType);
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(`http://192.168.29.195:3000/profile/$(userId)`);
+                const { user } = response.data;
+                setUser(user);
+            } catch(error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchProfile();
+    });
   return (
     <View>
       <Text>ProfileScreen</Text>
